4c3dd204323ef51abfb054bfb13c99723fd77bec
vlsulliv <vlsulliv@yahoo.com>
2021-11-15 21:34:02 -0600

    remove unnecessary files

--- index.js
+++ index.js
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-const inquirer = require('inquirer');
-const fs = require('fs');
-
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name?',
-    },
-    {
-      type: 'checkbox',
-      message: 'What languages do you know?',
-      name: 'stack',
-      choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
-    },
-    {
-      type: 'list',
-      message: 'What is your preferred method of communication?',
-      name: 'contact',
-      choices: ['email', 'phone', 'telekinesis'],
-    },
-  ])
-  .then((data) => {
-    const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
-
-    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
-      err ? console.log(err) : console.log('Success!')
-    );
-  });
-
-=======
 // imported packages
 const fs = require('fs');
 const inquirer = require('inquirer');
@@ -115,4 +82,3 @@ function init() {
 
 // initialize app
 init();
->>>>>>> 5016f49 (format generated markdown - specify how information will be diplayed on created markdown)
